f5671d2003113ad8bb8b92793fc06fbfbdc00019
Brodie Slater <brodster22@live.com>
2023-03-20 06:24:20 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/google/go-github/v50/github"
+	"golang.org/x/oauth2"
+)
+
+type GH struct {
+	client *github.Client
+}
+
+func run(teamName1, teamName2, token string) {
+	if teamName1 == "" || teamName2 == "" {
+		log.Fatalln("Invalid number of arguments! Two team names need to be provided")
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	gh := GH{
+		client: client,
+	}
+
+	inBoth, err := gh.compare(ctx, teamName1, teamName2)
+	if err != nil {
+		log.Fatalf("Something went wrong:\n%v\n", err)
+	}
+
+	for _, m := range inBoth {
+		login := *m.Login
+		var name string
+		var email string
+		if m.Name == nil {
+			name = "empty"
+		} else {
+			name = *m.Name
+		}
+		if m.Email == nil {
+			email = "empty"
+		} else {
+			email = *m.Email
+		}
+		fmt.Printf("%-20s %-20s %s\n", login, name, email)
+	}
+}
+
+// Lookup team info
+// Get team membership of both teams
+// return members missing from second team
+func (gh GH) compare(ctx context.Context, slug1, slug2 string) ([]*github.User, error) {
+	split1 := strings.Split(slug1, "/")
+	org1 := split1[0]
+	team1 := split1[1]
+	team1Details, _, err := gh.client.Teams.ListTeamMembersBySlug(ctx, org1, team1, &github.TeamListTeamMembersOptions{})
+	if err != nil {
+		return []*github.User{}, err
+	}
+
+	split2 := strings.Split(slug2, "/")
+	org2 := split2[0]
+	team2 := split2[1]
+	team2Details, _, err := gh.client.Teams.ListTeamMembersBySlug(ctx, org2, team2, &github.TeamListTeamMembersOptions{})
+	if err != nil {
+		return []*github.User{}, err
+	}
+
+	var membersInBothTeams []*github.User
+	for _, m1 := range team1Details {
+		present := false
+		for _, m2 := range team2Details {
+			if *m1.Login == *m2.Login {
+				present = true
+			}
+		}
+		if !present {
+			membersInBothTeams = append(membersInBothTeams, m1)
+		}
+	}
+	return membersInBothTeams, nil
+}
+
+func main() {
+	var team1 *string = flag.String("t1", "", "The slug of the base GH team to compare membership: (org/teamName)")
+	var team2 *string = flag.String("t2", "", "The slug of the targe GH team to compare membership: (org/TeamName)")
+	var token *string = flag.String("token", "", "A GitHub auth token")
+	flag.Parse()
+
+	run(*team1, *team2, *token)
+}
